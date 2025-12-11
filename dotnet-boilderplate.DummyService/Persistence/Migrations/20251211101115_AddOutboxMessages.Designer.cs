e6206151cedaa5de1a23e56f1374ffbf29eff810
nghiant2 <nghiant2@PC-NGHIANT2>
2025-12-11 17:18:07 +0700

    feat: add migrations outbox message

--- dotnet-boilderplate.DummyService/Persistence/Migrations/20251211101115_AddOutboxMessages.Designer.cs
+++ dotnet-boilderplate.DummyService/Persistence/Migrations/20251211101115_AddOutboxMessages.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using dotnet_boilderplate.DummyService.Persistence;
@@ -11,9 +12,11 @@ using dotnet_boilderplate.DummyService.Persistence;
 namespace dotnet_boilderplate.DummyService.Persistence.Migrations
 {
     [DbContext(typeof(DummyDbContext))]
-    partial class DummyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251211101115_AddOutboxMessages")]
+    partial class AddOutboxMessages
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ dotnet-boilderplate.DummyService/Persistence/Migrations/20251211101115_AddOutboxMessages.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnet_boilderplate.DummyService.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOutboxMessages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "outbox_message",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    type = table.Column<string>(type: "text", nullable: false),
+                    payload = table.Column<string>(type: "text", nullable: false),
+                    published = table.Column<bool>(type: "boolean", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamptz", nullable: false),
+                    created_by = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    updated_at = table.Column<DateTime>(type: "timestamptz", nullable: true),
+                    updated_by = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_outbox_message", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "outbox_message");
+        }
+    }
+}
